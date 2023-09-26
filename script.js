9cef40eacac1a8489e3b1a29bcfb290b0c7462f2
Balazs Gergo <gergobalazs98@gmail.com>
2023-09-26 12:34:55 +0200

    variables

---
+++ script.js
@@ -0,0 +1,11 @@
+let variable = "kis"
+let variable2 = "macska"
+
+let number1 = 5
+let number2 = 10
+
+//console.log("100" + "2")
+//console.log(100 + 2)
+console.log("variable")
+console.log(variable + variable2)
+console.log(number1 + number2)
