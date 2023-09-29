c5182f4f97b9167ad654ba051df542ee3bedd169
Balazs Gergo <gergobalazs98@gmail.com>
2023-09-29 11:43:41 +0200

    full practice loop

--- script.js
+++ script.js
@@ -1,43 +1,67 @@
-let x = 10
-let y = "text"
-let z = true
+let x = 10 // number
+let y = "text" // string
+let z = true // boolean
 
-let arr = ['one', 'two', 'three', 'four']
-let arr2 = [10,5,67,234]
-let arr3 = ['text',67,false,true]
+let arr = ["one", "two", "three", "four"] // array
 
+let arrObj = {
+  0: "one",
+  1: "two",
+  2: "three",
+  3: "four"
+}
 
-let newArr = [arr,arr2,arr3]
+let arr2 = [10, 5, 67, 234] // array
+let arr3 = ["text", 67, false, null] // array
+
+let newArr = [arr, arr2, arr3] // array
 
 //console.log(newArr[0][0])
 
-
 let obj = {
-    key: 'value',
-    key2: 20,
-    key3: false,
-    key4: ['egy','ketto','harom'],
-    key5: {
-        nestedKey: 'nested value'
-    }
-
+  key4: ['egy', `kettő${x}`, "három \"négy\""],
+  key: "value",
+  key2: 20,
+  key3: false,
+  key5: {
+    nestedKey: 'nested value'
+  }
 }
-//console.log(obj.key4)
 
 let jsonExample = [
-{
-   name: 'Gipsz Jakab',
-   age: 40,
-},
-
-{ name: 'pelda peter',
-age: 28,
-},
-   
-{name: 'john doe',
-age: 6,}
-
-   
+  {
+    "name": "Gipsz Jakab",
+    "age": 40,
+    "hair-count": 10,
+    "zokni-count": 20
+  },
+  {
+    "name": "Példa Péter",
+    "age": 11,
+    "hair-count": 0,
+    "zokni-count": 12
+  },
+  {
+    "name": "John Doe",
+    "age": 6,
+    "hair-count": 200,
+    "zokni-count": 37
+  }
 ]
 
-console.log(jsonExample)
+for (let i = 0; i < jsonExample.length; i++) {
+  //let element = jsonExample[i]
+  let user = jsonExample[i]
+
+  /* if (user.age > 18 && user["hair-count"] > 11) {
+    console.log(`${user.name} kapni fog reklámot`)
+  } else {
+    console.log(`${user.name} nem használhatja a sampont`)
+  } */
+
+  if (user["zokni-count"] % 2 === 1) {
+    console.log(`sell ${user.name} a single zokni`)
+  } else {
+    console.log(`${user.name} sadly has exact pair of zoknis`)
+  }
+}
