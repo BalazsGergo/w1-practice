7776b2073fe79cb677939411f65a5fd3d18e99f4
Balazs Gergo <gergobalazs98@gmail.com>
2023-09-29 11:21:13 +0200

    object-json practice

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-console.log('Hello World')
+let x = 10
+let y = "text"
+let z = true
 
-for (let i = i; i <= 5; i++){
-    console.log(i)
+let arr = ['one', 'two', 'three', 'four']
+let arr2 = [10,5,67,234]
+let arr3 = ['text',67,false,true]
+
+
+let newArr = [arr,arr2,arr3]
+
+//console.log(newArr[0][0])
+
+
+let obj = {
+    key: 'value',
+    key2: 20,
+    key3: false,
+    key4: ['egy','ketto','harom'],
+    key5: {
+        nestedKey: 'nested value'
+    }
+
 }
+//console.log(obj.key4)
+
+let jsonExample = [
+{
+   name: 'Gipsz Jakab',
+   age: 40,
+},
+
+{ name: 'pelda peter',
+age: 28,
+},
+   
+{name: 'john doe',
+age: 6,}
+
+   
+]
+
+console.log(jsonExample)
