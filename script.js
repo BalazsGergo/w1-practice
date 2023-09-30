6bb1e0af32bb0e2a9898c8e480531b3b8f7c9c4c
Balazs Gergo <gergobalazs98@gmail.com>
2023-09-30 14:46:43 +0200

    practice

--- script.js
+++ script.js
@@ -1,67 +1,28 @@
-let x = 10 // number
-let y = "text" // string
-let z = true // boolean
 
-let arr = ["one", "two", "three", "four"] // array
+let champions = [
+ {
+  "name": 'Azir',
+  "winrt": 50 
+ },
 
-let arrObj = {
-  0: "one",
-  1: "two",
-  2: "three",
-  3: "four"
-}
+ {
+  "name": 'Akali',
+  "winrt": 49 
+ },
 
-let arr2 = [10, 5, 67, 234] // array
-let arr3 = ["text", 67, false, null] // array
+ {
+  "name": 'Zed',
+  "winrt": 52 
+ }
 
-let newArr = [arr, arr2, arr3] // array
-
-//console.log(newArr[0][0])
-
-let obj = {
-  key4: ['egy', `kettő${x}`, "három \"négy\""],
-  key: "value",
-  key2: 20,
-  key3: false,
-  key5: {
-    nestedKey: 'nested value'
-  }
-}
-
-let jsonExample = [
-  {
-    "name": "Gipsz Jakab",
-    "age": 40,
-    "hair-count": 10,
-    "zokni-count": 20
-  },
-  {
-    "name": "Példa Péter",
-    "age": 11,
-    "hair-count": 0,
-    "zokni-count": 12
-  },
-  {
-    "name": "John Doe",
-    "age": 6,
-    "hair-count": 200,
-    "zokni-count": 37
-  }
 ]
 
-for (let i = 0; i < jsonExample.length; i++) {
-  //let element = jsonExample[i]
-  let user = jsonExample[i]
 
-  /* if (user.age > 18 && user["hair-count"] > 11) {
-    console.log(`${user.name} kapni fog reklámot`)
-  } else {
-    console.log(`${user.name} nem használhatja a sampont`)
-  } */
+for (i = 0; i < champions.length;i++) {
+  let element = champions[i]
 
-  if (user["zokni-count"] % 2 === 1) {
-    console.log(`sell ${user.name} a single zokni`)
-  } else {
-    console.log(`${user.name} sadly has exact pair of zoknis`)
+  // console.log(element.winrt + '%')
+  if (element.winrt >= 50 && element.name.length === 4) {
+    console.log(` ${element.name} this champ is op!!!!`)
   }
 }
