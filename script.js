2d20c08c7dca9cc0f81e21e83a492917d22e8031
Balazs Gergo <gergobalazs98@gmail.com>
2023-10-02 12:59:55 +0200

    function and if-else practice

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-
-let champions = [
- {
-  "name": 'Azir',
-  "winrt": 50 
- },
-
- {
-  "name": 'Akali',
-  "winrt": 49 
- },
-
- {
-  "name": 'Zed',
-  "winrt": 52 
- }
-
-]
-
-
-for (i = 0; i < champions.length;i++) {
-  let element = champions[i]
-
-  // console.log(element.winrt + '%')
-  if (element.winrt >= 50 && element.name.length === 4) {
-    console.log(` ${element.name} this champ is op!!!!`)
+function sumDouble(a, b){
+    let result = a + b
+    if (a != b ){
+    return result 
+    } else {
+     return result * 2
+    }
   }
-}
+  console.log(sumDouble(3,4))
